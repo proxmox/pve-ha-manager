proxmox/pve-ha-manager

commit dcb4a2a48404a8bf06df41e071fea348d0c971a4

get_verbose_service_state: render removal transition as 'deleting'
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/API2/HA/Status.pm
@@ -154,7 +154,7 @@ __PACKAGE__->register_method ({
 	    if ($ss) {
 		$data->{node} = $ss->{node};
 		$data->{crm_state} = $ss->{state};
-	    } else {
+	    } elsif ($ss) {
 		$data->{node} = $sc->{node};
 	    }
 	    my $node = $data->{node} // '---'; # to be save against manual tinkering

src/PVE/HA/Tools.pm
@@ -130,6 +130,8 @@ sub count_fenced_services {
 sub get_verbose_service_state {
     my ($service_state, $service_conf) = @_;
 
+    return 'deleting' if !$service_conf;
+
     my $req = $service_conf->{state} // 'ignored';
     return 'ignored' if $req eq 'ignored';
 

commit c9b21b5a0bf7288bd437d2b02d87435438884601

followup: s/ss/sc/
fixes: dcb4a2a48404a8bf06df41e071fea348d0c971a4

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/API2/HA/Status.pm
@@ -154,7 +154,7 @@ __PACKAGE__->register_method ({
 	    if ($ss) {
 		$data->{node} = $ss->{node};
 		$data->{crm_state} = $ss->{state};
-	    } elsif ($ss) {
+	    } elsif ($sc) {
 		$data->{node} = $sc->{node};
 	    }
 	    my $node = $data->{node} // '---'; # to be save against manual tinkering