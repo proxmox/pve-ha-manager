c8c6e462fc67447014e6f3542beba72a2157fd14
Fiona Ebner <f.ebner@proxmox.com>
2022-11-17 15:00:04 +0100

    add Usage base plugin and Usage::Basic plugin
    in preparation to also support static resource scheduling via another
    such Usage plugin.
    
    The interface is designed in anticipation of the Usage::Static plugin,
    the Usage::Basic plugin doesn't require all parameters.
    
    In Usage::Static, the $haenv will necessary for logging and getting
    the static node stats. add_service_usage_to_node() and
    score_nodes_to_start_service() take the sid, service node and the
    former also the optional migration target (during a migration it's not
    clear whether the config file has already been moved or not) to be
    able to get the static service stats.
    
    Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

---
+++ src/PVE/HA/Usage.pm
@@ -0,0 +1,49 @@
+package PVE::HA::Usage;
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, $haenv) = @_;
+
+    die "implement in subclass";
+}
+
+sub add_node {
+    my ($self, $nodename) = @_;
+
+    die "implement in subclass";
+}
+
+sub remove_node {
+    my ($self, $nodename) = @_;
+
+    die "implement in subclass";
+}
+
+sub list_nodes {
+    my ($self) = @_;
+
+    die "implement in subclass";
+}
+
+sub contains_node {
+    my ($self, $nodename) = @_;
+
+    die "implement in subclass";
+}
+
+sub add_service_usage_to_node {
+    my ($self, $nodename, $sid, $service_node, $migration_target) = @_;
+
+    die "implement in subclass";
+}
+
+# Returns a hash with $nodename => $score pairs. A lower $score is better.
+sub score_nodes_to_start_service {
+    my ($self, $sid, $service_node) = @_;
+
+    die "implement in subclass";
+}
+
+1;
---
+++ src/PVE/HA/Usage/Basic.pm
@@ -0,0 +1,52 @@
+package PVE::HA::Usage::Basic;
+
+use strict;
+use warnings;
+
+use base qw(PVE::HA::Usage);
+
+sub new {
+    my ($class, $haenv) = @_;
+
+    return bless {
+	nodes => {},
+    }, $class;
+}
+
+sub add_node {
+    my ($self, $nodename) = @_;
+
+    $self->{nodes}->{$nodename} = 0;
+}
+
+sub remove_node {
+    my ($self, $nodename) = @_;
+
+    delete $self->{nodes}->{$nodename};
+}
+
+sub list_nodes {
+    my ($self) = @_;
+
+    return keys $self->{nodes}->%*;
+}
+
+sub contains_node {
+    my ($self, $nodename) = @_;
+
+    return defined($self->{nodes}->{$nodename});
+}
+
+sub add_service_usage_to_node {
+    my ($self, $nodename, $sid, $service_node, $migration_target) = @_;
+
+    $self->{nodes}->{$nodename}++;
+}
+
+sub score_nodes_to_start_service {
+    my ($self, $sid, $service_node) = @_;
+
+    return $self->{nodes};
+}
+
+1;
