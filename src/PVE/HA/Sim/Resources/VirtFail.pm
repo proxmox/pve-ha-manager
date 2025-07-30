d9dfa4e2896f383774769a0548ece9a19fd78973
Daniel Kral <d.kral@proxmox.com>
2025-07-30 20:14:13 +0200

    sim: resources: add option to limit start and migrate tries to node
    Add an option to the VirtFail's name to allow the start and migrate fail
    counts to only apply on a certain node number with a specific naming
    scheme.
    
    This allows a slightly more elaborate test type, e.g. where a service
    can start on one node (or any other in that case), but fails to start on
    a specific node, which it is expected to start on after a migration.
    
    Signed-off-by: Daniel Kral <d.kral@proxmox.com>
    Link: https://lore.proxmox.com/20250730181428.392906-10-d.kral@proxmox.com

--- src/PVE/HA/Sim/Resources/VirtFail.pm
+++ src/PVE/HA/Sim/Resources/VirtFail.pm
@@ -13,24 +13,28 @@ use base qw(PVE::HA::Sim::Resources);
 #   fa:abc
 #   fa:abcd
 #   fa:abcde
+#   fa:abcdef
 # And the digits after the fa: type prefix would mean:
 #   - a: no meaning but can be used for differentiating similar resources
 #   - b: how many tries are needed to start correctly (0 is normal behavior) (should be set)
 #   - c: how many tries are needed to migrate correctly (0 is normal behavior) (should be set)
 #   - d: should shutdown be successful (0 = yes, anything else no) (optional)
 #   - e: return value of $plugin->exists() defaults to 1 if not set (optional)
+#   - f: limits the constraints of b and c to the nodeX (0 = apply to all nodes) (optional)
 
 my $decode_id = sub {
     my $id = shift;
 
-    my ($start, $migrate, $stop, $exists) = $id =~ /^\d(\d)(\d)(\d)?(\d)?/g;
+    my ($start, $migrate, $stop, $exists, $limit_to_node) =
+        $id =~ /^\d(\d)(\d)(\d)?(\d)?(\d)?/g;
 
     $start = 0 if !defined($start);
     $migrate = 0 if !defined($migrate);
     $stop = 0 if !defined($stop);
     $exists = 1 if !defined($exists);
+    $limit_to_node = 0 if !defined($limit_to_node);
 
-    return ($start, $migrate, $stop, $exists);
+    return ($start, $migrate, $stop, $exists, $limit_to_node);
 };
 
 my $tries = {
@@ -54,12 +58,14 @@ sub exists {
 sub start {
     my ($class, $haenv, $id) = @_;
 
-    my ($start_failure_count) = &$decode_id($id);
+    my ($start_failure_count, $limit_to_node) = ($decode_id->($id))[0, 4];
 
-    $tries->{start}->{$id} = 0 if !$tries->{start}->{$id};
-    $tries->{start}->{$id}++;
+    if ($limit_to_node == 0 || $haenv->nodename() eq "node$limit_to_node") {
+        $tries->{start}->{$id} = 0 if !$tries->{start}->{$id};
+        $tries->{start}->{$id}++;
 
-    return if $start_failure_count >= $tries->{start}->{$id};
+        return if $start_failure_count >= $tries->{start}->{$id};
+    }
 
     $tries->{start}->{$id} = 0; # reset counts
 
@@ -80,12 +86,14 @@ sub shutdown {
 sub migrate {
     my ($class, $haenv, $id, $target, $online) = @_;
 
-    my (undef, $migrate_failure_count) = &$decode_id($id);
+    my ($migrate_failure_count, $limit_to_node) = ($decode_id->($id))[1, 4];
 
-    $tries->{migrate}->{$id} = 0 if !$tries->{migrate}->{$id};
-    $tries->{migrate}->{$id}++;
+    if ($limit_to_node == 0 || $haenv->nodename() eq "node$limit_to_node") {
+        $tries->{migrate}->{$id} = 0 if !$tries->{migrate}->{$id};
+        $tries->{migrate}->{$id}++;
 
-    return if $migrate_failure_count >= $tries->{migrate}->{$id};
+        return if $migrate_failure_count >= $tries->{migrate}->{$id};
+    }
 
     $tries->{migrate}->{$id} = 0; # reset counts
 
