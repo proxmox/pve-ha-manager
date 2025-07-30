272525b105342a75d22a5c83c9cd5c37585de2b3
Daniel Kral <d.kral@proxmox.com>
2025-07-30 19:59:39 +0200

    test: add test cases for rules config
    Add test cases to verify that the rule checkers correctly identify and
    remove HA rules from the rules to make the rule set feasible. For now,
    there only are HA Node Affinity rules, which verify:
    
    - Node Affinity rules retrieve the correct optional default values
    - Node Affinity rules, which specify the same HA resource more than
      once, are dropped from the rule set
    
    Signed-off-by: Daniel Kral <d.kral@proxmox.com>
    Link: https://lore.proxmox.com/20250730175957.386674-13-d.kral@proxmox.com
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

---
+++ src/test/test_rules_config.pl
@@ -0,0 +1,100 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+use lib qw(..);
+
+use Test::More;
+use Test::MockModule;
+
+use Data::Dumper;
+
+use PVE::HA::Rules;
+use PVE::HA::Rules::NodeAffinity;
+
+PVE::HA::Rules::NodeAffinity->register();
+
+PVE::HA::Rules->init(property_isolation => 1);
+
+my $opt_nodiff;
+
+if (!GetOptions("nodiff" => \$opt_nodiff)) {
+    print "usage: $0 [test.cfg] [--nodiff]\n";
+    exit -1;
+}
+
+sub _log {
+    my ($fh, $source, $message) = @_;
+
+    chomp $message;
+    $message = "[$source] $message" if $source;
+
+    print "$message\n";
+
+    $fh->print("$message\n");
+    $fh->flush();
+}
+
+sub check_cfg {
+    my ($cfg_fn, $outfile) = @_;
+
+    my $raw = PVE::Tools::file_get_contents($cfg_fn);
+
+    open(my $LOG, '>', "$outfile");
+    select($LOG);
+    $| = 1;
+
+    print "--- Log ---\n";
+    my $cfg = PVE::HA::Rules->parse_config($cfg_fn, $raw);
+    PVE::HA::Rules->set_rule_defaults($_) for values %{ $cfg->{ids} };
+    my $messages = PVE::HA::Rules->canonicalize($cfg);
+    print $_ for @$messages;
+    print "--- Config ---\n";
+    {
+        local $Data::Dumper::Sortkeys = 1;
+        print Dumper($cfg);
+    }
+
+    select(STDOUT);
+}
+
+sub run_test {
+    my ($cfg_fn) = @_;
+
+    print "* check: $cfg_fn\n";
+
+    my $outfile = "$cfg_fn.output";
+    my $expect = "$cfg_fn.expect";
+
+    eval { check_cfg($cfg_fn, $outfile); };
+    if (my $err = $@) {
+        die "Test '$cfg_fn' failed:\n$err\n";
+    }
+
+    return if $opt_nodiff;
+
+    my $res;
+
+    if (-f $expect) {
+        my $cmd = ['diff', '-u', $expect, $outfile];
+        $res = system(@$cmd);
+        die "test '$cfg_fn' failed\n" if $res != 0;
+    } else {
+        $res = system('cp', $outfile, $expect);
+        die "test '$cfg_fn' failed\n" if $res != 0;
+    }
+
+    print "* end rules test: $cfg_fn (success)\n\n";
+}
+
+# exec tests
+
+if (my $testcfg = shift) {
+    run_test($testcfg);
+} else {
+    for my $cfg (<rules_cfgs/*cfg>) {
+        run_test($cfg);
+    }
+}
