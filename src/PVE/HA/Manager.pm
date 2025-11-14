proxmox/pve-ha-manager

commit 6c4c04584643a76078ae5561376655519cde48c5

rules: add haenv node list to the rules' canonicalization stage
Add the HA environment's node list information to the feasibility
check/canonicalization stage, which is needed for at least one rule
check for negative resource affinity rules in an upcoming patch, which
verifies that there are enough available nodes to separate the HA
resources on.

Signed-off-by: Daniel Kral <d.kral@proxmox.com>
Link: https://lore.proxmox.com/20250730181428.392906-4-d.kral@proxmox.com

src/PVE/HA/Manager.pm
@@ -688,7 +688,8 @@ sub manage {
     ) {
         PVE::HA::Groups::migrate_groups_to_rules($new_rules, $self->{groups}, $sc);
 
-        my $messages = PVE::HA::Rules->canonicalize($new_rules);
+        my $nodes = $self->{ns}->list_nodes();
+        my $messages = PVE::HA::Rules->canonicalize($new_rules, $nodes);
         $haenv->log('info', $_) for @$messages;
 
         $self->{rules} = $new_rules;

src/PVE/API2/HA/Rules.pm
@@ -98,7 +98,10 @@ my $check_feasibility = sub {
 
     $rules = $get_full_rules_config->($rules);
 
-    return PVE::HA::Rules->check_feasibility($rules);
+    my $manager_status = PVE::HA::Config::read_manager_status();
+    my $nodes = [keys $manager_status->{node_status}->%*];
+
+    return PVE::HA::Rules->check_feasibility($rules, $nodes);
 };
 
 my $assert_feasibility = sub {

src/PVE/HA/Rules.pm
@@ -322,11 +322,13 @@ sub get_check_arguments : prototype($$) {
     return $global_args;
 }
 
-=head3 $class->check_feasibility($rules)
+=head3 $class->check_feasibility($rules, $nodes)
 
 Checks whether the given C<$rules> are feasible by running all checks, which
 were registered with C<L<< register_check()|/$class->register_check(...) >>>,
 and returns a hash map of errorneous rules.
+
+C<$nodes> is a list of the configured cluster nodes.
 
 The checks are run in the order in which the rule plugins were registered,
 while global checks, i.e. checks between different rule types, are run at the
@@ -334,13 +336,15 @@ very last.
 
 =cut
 
-sub check_feasibility : prototype($$) {
-    my ($class, $rules) = @_;
+sub check_feasibility : prototype($$$) {
+    my ($class, $rules, $nodes) = @_;
 
     my $global_errors = {};
     my $removable_ruleids = [];
 
     my $global_args = $class->get_check_arguments($rules);
+
+    $global_args->{nodes} = $nodes;
 
     for my $type (@$types, 'global') {
         for my $entry (@{ $checkdef->{$type} }) {
@@ -366,9 +370,11 @@ sub plugin_canonicalize : prototype($$) {
     my ($class, $rules) = @_;
 }
 
-=head3 $class->canonicalize($rules)
+=head3 $class->canonicalize($rules, $nodes)
 
 Modifies C<$rules> to contain only feasible rules.
+
+C<$nodes> is a list of the configured cluster nodes.
 
 This is done by running all checks, which were registered with
 C<L<< register_check()|/$class->register_check(...) >>> and removing any
@@ -378,11 +384,11 @@ Returns a list of messages with the reasons why rules were removed.
 
 =cut
 
-sub canonicalize : prototype($$) {
-    my ($class, $rules) = @_;
+sub canonicalize : prototype($$$) {
+    my ($class, $rules, $nodes) = @_;
 
     my $messages = [];
-    my $global_errors = $class->check_feasibility($rules);
+    my $global_errors = $class->check_feasibility($rules, $nodes);
 
     for my $ruleid (keys %$global_errors) {
         delete $rules->{ids}->{$ruleid};

src/test/test_rules_config.pl
@@ -42,6 +42,8 @@ sub check_cfg {
 
     my $raw = PVE::Tools::file_get_contents($cfg_fn);
 
+    my $nodes = ['node1', 'node2', 'node3'];
+
     open(my $LOG, '>', "$outfile");
     select($LOG);
     $| = 1;
@@ -49,7 +51,7 @@ sub check_cfg {
     print "--- Log ---\n";
     my $cfg = PVE::HA::Rules->parse_config($cfg_fn, $raw);
     PVE::HA::Rules->set_rule_defaults($_) for values %{ $cfg->{ids} };
-    my $messages = PVE::HA::Rules->canonicalize($cfg);
+    my $messages = PVE::HA::Rules->canonicalize($cfg, $nodes);
     print $_ for @$messages;
     print "--- Config ---\n";
     {

commit e1d525d3cef37e782410ae5c170ca4a71f58b3e1

manager: retranslate rules if nodes are added or removed
Some rule checks depend on the list of cluster nodes, e.g., to check
whether a negative resource affinity rule doesn't specify more HA
resources than cluster nodes.

The HA Manager retranslates rules only in certain conditions to reduce
unnecessary computations, but lacks a check whether cluster nodes have
been added or removed, which is different from what users are reported
through the rules API endpoints and web interface.

Fixes: 6c4c0458 ("rules: add haenv node list to the rules' canonicalization stage")
Signed-off-by: Daniel Kral <d.kral@proxmox.com>
Reviewed-by: Michael Köppl <m.koeppl@proxmox.com>
Tested-by: Michael Köppl <m.koeppl@proxmox.com>
Link: https://lore.proxmox.com/20251114111828.160294-3-d.kral@proxmox.com

src/PVE/HA/Manager.pm
@@ -664,6 +664,7 @@ sub manage {
     my ($haenv, $ms, $ns, $ss) = ($self->{haenv}, $self->{ms}, $self->{ns}, $self->{ss});
 
     my ($node_info) = $haenv->get_node_info();
+    my $has_changed_nodelist = $ns->check_for_changed_nodelist($node_info);
     my ($lrm_results, $lrm_modes) = $self->read_lrm_status();
 
     $ns->update($node_info, $lrm_modes);
@@ -719,6 +720,7 @@ sub manage {
 
     if (
         !$self->{rules}
+        || $has_changed_nodelist
         || $new_rules->{digest} ne $self->{last_rules_digest}
         || $self->{groups}->{digest} ne $self->{last_groups_digest}
         || $services_digest && $services_digest ne $self->{last_services_digest}

src/PVE/HA/NodeStatus.pm
@@ -90,6 +90,20 @@ sub list_online_nodes {
     return $res;
 }
 
+sub check_for_changed_nodelist {
+    my ($self, $node_info) = @_;
+
+    for my $node (keys %$node_info) {
+        return 1 if !$self->{status}->{$node};
+    }
+
+    for my $node (keys $self->{status}->%*) {
+        return 1 if !$node_info->{$node};
+    }
+
+    return 0;
+}
+
 my $delete_node = sub {
     my ($self, $node) = @_;
 