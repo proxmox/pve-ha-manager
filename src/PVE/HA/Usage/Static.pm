proxmox/pve-ha-manager

commit e2feb84ce3523b722dcad6922e603649bb78a604

rename static node stats to be consistent with similar interfaces
The names `maxcpu` and `maxmem` are used in the static load scheduler
itself and is more telling that these properties provide the maximum
configured amount of CPU cores and memory.

Signed-off-by: Daniel Kral <d.kral@proxmox.com>
Link: https://lore.proxmox.com/20260319164047.642936-2-d.kral@proxmox.com
 [TL: switch to definedness check as per Dominik R.'s review]
Reviewed-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/HA/Usage/Static.pm
@@ -33,10 +33,10 @@ sub add_node {
 
     my $stats = $self->{'node-stats'}->{$nodename}
         or die "did not get static node usage information for '$nodename'\n";
-    die "static node usage information for '$nodename' missing cpu count\n" if !$stats->{cpus};
-    die "static node usage information for '$nodename' missing memory\n" if !$stats->{memory};
+    die "static node usage information for '$nodename' missing cpu count\n" if !defined($stats->{maxcpu});
+    die "static node usage information for '$nodename' missing memory\n" if !defined($stats->{maxmem});
 
-    eval { $self->{scheduler}->add_node($nodename, int($stats->{cpus}), int($stats->{memory})); };
+    eval { $self->{scheduler}->add_node($nodename, int($stats->{maxcpu}), int($stats->{maxmem})); };
     die "initializing static node usage for '$nodename' failed - $@" if $@;
 }
 

src/PVE/HA/Env/PVE2.pm
@@ -548,7 +548,14 @@ sub get_static_node_stats {
 
     my $stats = PVE::Cluster::get_node_kv('static-info');
     for my $node (keys $stats->%*) {
-        $stats->{$node} = eval { decode_json($stats->{$node}) };
+        $stats->{$node} = eval {
+            my $node_stats = decode_json($stats->{$node});
+
+            return {
+                maxcpu => $node_stats->{cpus},
+                maxmem => $node_stats->{memory},
+            };
+        };
         $self->log('err', "unable to decode static node info for '$node' - $@") if $@;
     }
 

src/PVE/HA/Sim/Hardware.pm
@@ -488,9 +488,9 @@ sub new {
             || die "Copy failed: $!\n";
     } else {
         my $cstatus = {
-            node1 => { power => 'off', network => 'off', cpus => 24, memory => 131072 },
-            node2 => { power => 'off', network => 'off', cpus => 24, memory => 131072 },
-            node3 => { power => 'off', network => 'off', cpus => 24, memory => 131072 },
+            node1 => { power => 'off', network => 'off', maxcpu => 24, maxmem => 131072 },
+            node2 => { power => 'off', network => 'off', maxcpu => 24, maxmem => 131072 },
+            node3 => { power => 'off', network => 'off', maxcpu => 24, maxmem => 131072 },
         };
         $self->write_hardware_status_nolock($cstatus);
     }
@@ -1088,7 +1088,7 @@ sub get_static_node_stats {
 
     my $stats = {};
     for my $node (keys $cstatus->%*) {
-        $stats->{$node} = { $cstatus->{$node}->%{qw(cpus memory)} };
+        $stats->{$node} = { $cstatus->{$node}->%{qw(maxcpu maxmem)} };
     }
 
     return $stats;

src/test/test-crs-static-rebalance-resource-affinity1/hardware_status
@@ -1,5 +1,5 @@
 {
-  "node1": { "power": "off", "network": "off", "cpus": 8, "memory": 112000000000 },
-  "node2": { "power": "off", "network": "off", "cpus": 8, "memory": 112000000000 },
-  "node3": { "power": "off", "network": "off", "cpus": 8, "memory": 112000000000 }
+  "node1": { "power": "off", "network": "off", "maxcpu": 8, "maxmem": 112000000000 },
+  "node2": { "power": "off", "network": "off", "maxcpu": 8, "maxmem": 112000000000 },
+  "node3": { "power": "off", "network": "off", "maxcpu": 8, "maxmem": 112000000000 }
 }

src/test/test-crs-static-rebalance-resource-affinity2/hardware_status
@@ -1,5 +1,5 @@
 {
-  "node1": { "power": "off", "network": "off", "cpus": 8, "memory": 112000000000 },
-  "node2": { "power": "off", "network": "off", "cpus": 8, "memory": 112000000000 },
-  "node3": { "power": "off", "network": "off", "cpus": 8, "memory": 112000000000 }
+  "node1": { "power": "off", "network": "off", "maxcpu": 8, "maxmem": 112000000000 },
+  "node2": { "power": "off", "network": "off", "maxcpu": 8, "maxmem": 112000000000 },
+  "node3": { "power": "off", "network": "off", "maxcpu": 8, "maxmem": 112000000000 }
 }

src/test/test-crs-static-rebalance-resource-affinity3/hardware_status
@@ -1,7 +1,7 @@
 {
-  "node1": { "power": "off", "network": "off", "cpus": 8, "memory": 48000000000 },
-  "node2": { "power": "off", "network": "off", "cpus": 32, "memory": 36000000000 },
-  "node3": { "power": "off", "network": "off", "cpus": 16, "memory": 24000000000 },
-  "node4": { "power": "off", "network": "off", "cpus": 32, "memory": 36000000000 },
-  "node5": { "power": "off", "network": "off", "cpus": 8, "memory": 48000000000 }
+  "node1": { "power": "off", "network": "off", "maxcpu": 8, "maxmem": 48000000000 },
+  "node2": { "power": "off", "network": "off", "maxcpu": 32, "maxmem": 36000000000 },
+  "node3": { "power": "off", "network": "off", "maxcpu": 16, "maxmem": 24000000000 },
+  "node4": { "power": "off", "network": "off", "maxcpu": 32, "maxmem": 36000000000 },
+  "node5": { "power": "off", "network": "off", "maxcpu": 8, "maxmem": 48000000000 }
 }

src/test/test-crs-static-rebalance1/hardware_status
@@ -1,5 +1,5 @@
 {
-  "node1": { "power": "off", "network": "off", "cpus": 32, "memory": 256000000000 },
-  "node2": { "power": "off", "network": "off", "cpus": 32, "memory": 256000000000 },
-  "node3": { "power": "off", "network": "off", "cpus": 32, "memory": 256000000000 }
+  "node1": { "power": "off", "network": "off", "maxcpu": 32, "maxmem": 256000000000 },
+  "node2": { "power": "off", "network": "off", "maxcpu": 32, "maxmem": 256000000000 },
+  "node3": { "power": "off", "network": "off", "maxcpu": 32, "maxmem": 256000000000 }
 }

src/test/test-crs-static-rebalance2/hardware_status
@@ -1,5 +1,5 @@
 {
-  "node1": { "power": "off", "network": "off", "cpus": 40, "memory": 384000000000 },
-  "node2": { "power": "off", "network": "off", "cpus": 32, "memory": 256000000000 },
-  "node3": { "power": "off", "network": "off", "cpus": 32, "memory": 256000000000 }
+  "node1": { "power": "off", "network": "off", "maxcpu": 40, "maxmem": 384000000000 },
+  "node2": { "power": "off", "network": "off", "maxcpu": 32, "maxmem": 256000000000 },
+  "node3": { "power": "off", "network": "off", "maxcpu": 32, "maxmem": 256000000000 }
 }

commit b744a6d61ab355490cf5c70a48708db2431770a1

re-tidy perl code
Fixes: e2feb84 ("rename static node stats to be consistent with similar interfaces")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/HA/Usage/Static.pm
@@ -33,8 +33,10 @@ sub add_node {
 
     my $stats = $self->{'node-stats'}->{$nodename}
         or die "did not get static node usage information for '$nodename'\n";
-    die "static node usage information for '$nodename' missing cpu count\n" if !defined($stats->{maxcpu});
-    die "static node usage information for '$nodename' missing memory\n" if !defined($stats->{maxmem});
+    die "static node usage information for '$nodename' missing cpu count\n"
+        if !defined($stats->{maxcpu});
+    die "static node usage information for '$nodename' missing memory\n"
+        if !defined($stats->{maxmem});
 
     eval { $self->{scheduler}->add_node($nodename, int($stats->{maxcpu}), int($stats->{maxmem})); };
     die "initializing static node usage for '$nodename' failed - $@" if $@;
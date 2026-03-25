b744a6d61ab355490cf5c70a48708db2431770a1
Thomas Lamprecht <t.lamprecht@proxmox.com>
2026-03-25 13:04:13 +0100

    re-tidy perl code
    Fixes: e2feb84 ("rename static node stats to be consistent with similar interfaces")
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/PVE/HA/Usage/Static.pm
+++ src/PVE/HA/Usage/Static.pm
@@ -33,8 +33,10 @@ sub add_node {
 
     my $stats = $self->{'node-stats'}->{$nodename}
         or die "did not get static node usage information for '$nodename'\n";
-    die "static node usage information for '$nodename' missing cpu count\n" if !defined($stats->{maxcpu});
-    die "static node usage information for '$nodename' missing memory\n" if !defined($stats->{maxmem});
+    die "static node usage information for '$nodename' missing cpu count\n"
+        if !defined($stats->{maxcpu});
+    die "static node usage information for '$nodename' missing memory\n"
+        if !defined($stats->{maxmem});
 
     eval { $self->{scheduler}->add_node($nodename, int($stats->{maxcpu}), int($stats->{maxmem})); };
     die "initializing static node usage for '$nodename' failed - $@" if $@;
