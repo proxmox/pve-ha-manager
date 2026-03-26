proxmox/pve-ha-manager

commit a299f1a3259ce524bb9d75e39781539d50952f61

move static service stats repository to PVE::HA::Usage::Static
Since the static service stats are already processed by their individual
plugin's implementation of get_static_stats_from_config(...), the static
service stats repository can be used by the static load scheduler
directly.

Signed-off-by: Daniel Kral <d.kral@proxmox.com>
Link: https://lore.proxmox.com/20260319164047.642936-9-d.kral@proxmox.com

src/PVE/HA/Resources/PVECT.pm
@@ -168,10 +168,4 @@ sub get_static_stats_from_config {
     };
 }
 
-sub get_static_stats {
-    my ($class, $haenv, $sid) = @_;
-
-    return $haenv->get_static_service_stats($sid);
-}
-
 1;

src/PVE/HA/Resources/PVEVM.pm
@@ -189,10 +189,4 @@ sub get_static_stats_from_config {
     };
 }
 
-sub get_static_stats {
-    my ($class, $haenv, $sid) = @_;
-
-    return $haenv->get_static_service_stats($sid);
-}
-
 1;

src/PVE/HA/Env.pm
@@ -301,15 +301,9 @@ sub get_datacenter_settings {
 }
 
 sub get_static_service_stats {
-    my ($self, $id) = @_;
-
-    return $self->{plug}->get_static_service_stats($id);
-}
-
-sub update_static_service_stats {
     my ($self) = @_;
 
-    return $self->{plug}->update_static_service_stats();
+    return $self->{plug}->get_static_service_stats();
 }
 
 sub get_static_node_stats {

src/PVE/HA/Env/PVE2.pm
@@ -49,8 +49,6 @@ sub new {
 
     $self->{nodename} = $nodename;
 
-    $self->{static_service_stats} = undef;
-
     return $self;
 }
 
@@ -510,40 +508,26 @@ sub get_datacenter_settings {
 }
 
 sub get_static_service_stats {
-    my ($self, $id) = @_;
-
-    # undef if update_static_service_stats(...) failed before
-    return undef if !defined($self->{static_service_stats});
-
-    return $self->{static_service_stats}->{$id};
-}
-
-sub update_static_service_stats {
     my ($self) = @_;
 
     my $properties = ['cores', 'cpulimit', 'memory', 'sockets', 'vcpus'];
-    my $service_stats = eval {
-        my $stats = {};
-        my $confs = PVE::Cluster::get_guest_config_properties($properties);
+    my $stats = {};
+    my $confs = PVE::Cluster::get_guest_config_properties($properties);
 
-        my $vmlist = PVE::Cluster::get_vmlist();
-        my $idlist = $vmlist->{ids} // {};
-        for my $id (keys %$idlist) {
-            my $type = eval { PVE::HA::Tools::get_ha_resource_type($idlist->{$id}->{type}) };
-            next if $@; # silently ignore unknown pve types
+    my $vmlist = PVE::Cluster::get_vmlist();
+    my $idlist = $vmlist->{ids} // {};
+    for my $id (keys %$idlist) {
+        my $type = eval { PVE::HA::Tools::get_ha_resource_type($idlist->{$id}->{type}) };
+        next if $@; # silently ignore unknown pve types
 
-            my $sid = "$type:$id";
-            my $conf = $confs->{$id} // {};
-            my $plugin = PVE::HA::Resources->lookup($type);
+        my $sid = "$type:$id";
+        my $conf = $confs->{$id} // {};
+        my $plugin = PVE::HA::Resources->lookup($type);
 
-            $stats->{$sid} = $plugin->get_static_stats_from_config($conf);
-        }
+        $stats->{$sid} = $plugin->get_static_stats_from_config($conf);
+    }
 
-        return $stats;
-    };
-    $self->log('warning', "unable to update static service stats cache - $@") if $@;
-
-    $self->{static_service_stats} = $service_stats;
+    return $stats;
 }
 
 sub get_static_node_stats {

src/PVE/HA/Manager.pm
@@ -253,7 +253,6 @@ sub recompute_online_node_usage {
                 $online_node_usage = eval {
                     my $scheduler = PVE::HA::Usage::Static->new($haenv);
                     $scheduler->add_node($_) for $online_nodes->@*;
-                    $haenv->update_static_service_stats();
                     return $scheduler;
                 };
             } else {

src/PVE/HA/Resources.pm
@@ -183,12 +183,6 @@ sub get_static_stats_from_config {
     die "implement in subclass";
 }
 
-sub get_static_stats {
-    my ($class, $haenv, $sid) = @_;
-
-    die "implement in subclass";
-}
-
 # package PVE::HA::Resources::IPAddr;
 
 # use strict;

src/PVE/HA/Sim/Env.pm
@@ -489,15 +489,9 @@ sub get_datacenter_settings {
 }
 
 sub get_static_service_stats {
-    my ($self, $id) = @_;
-
-    return $self->{hardware}->get_static_service_stats($id);
-}
-
-sub update_static_service_stats {
     my ($self) = @_;
 
-    return $self->{hardware}->update_static_service_stats();
+    return $self->{hardware}->get_static_service_stats();
 }
 
 sub get_static_node_stats {

src/PVE/HA/Sim/Hardware.pm
@@ -525,8 +525,6 @@ sub new {
 
     $self->{service_config} = $self->read_service_config();
 
-    $self->{static_service_stats} = undef;
-
     return $self;
 }
 
@@ -1063,22 +1061,13 @@ sub watchdog_update {
 }
 
 sub get_static_service_stats {
-    my ($self, $id) = @_;
-
-    # undef if update_static_service_stats(...) failed before
-    return undef if !defined($self->{static_service_stats});
-
-    return $self->{static_service_stats}->{$id};
-}
-
-sub update_static_service_stats {
     my ($self) = @_;
 
     my $filename = "$self->{statusdir}/static_service_stats";
     my $stats = eval { PVE::HA::Tools::read_json_from_file($filename) };
     $self->log('warning', "unable to update static service stats cache - $@") if $@;
 
-    $self->{static_service_stats} = $stats;
+    return $stats;
 }
 
 sub get_static_node_stats {

commit 8328cd271652eb55c42f09df092914ef93e8bcef

resources: fix type confusion regression for static stats
Adding a container with an explicit number of 'cores' in the
configuration would fail with an error like:

> unable to add service 'ct:105' usage to node 'pve9a2'
> - error: invalid type: string "4", expected f64

Commit a299f1a ("move static service stats repository to
PVE::HA::Usage::Static") accidentally broke this by removing the
following hunk without adding new type coercions:

> -    my $service_stats = {
> -        maxcpu => $stats->{maxcpu} + 0.0, # containers allow non-integer cpulimit
> -        maxmem => int($stats->{maxmem}),
> -    };

Fixes: a299f1a ("move static service stats repository to PVE::HA::Usage::Static")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20260325150624.467936-1-f.ebner@proxmox.com

src/PVE/HA/Resources/PVECT.pm
@@ -162,9 +162,11 @@ sub remove_locks {
 sub get_static_stats_from_config {
     my ($class, $conf) = @_;
 
+    # Ensure types are compatible with what Rust expects: f64 for maxcpu and usize for maxmem.
+    # Note that there might be a non-integer 'cpulimit'.
     return {
-        maxcpu => PVE::LXC::Config->get_derived_property($conf, 'max-cpu'),
-        maxmem => PVE::LXC::Config->get_derived_property($conf, 'max-memory'),
+        maxcpu => 0.0 + PVE::LXC::Config->get_derived_property($conf, 'max-cpu'),
+        maxmem => int(PVE::LXC::Config->get_derived_property($conf, 'max-memory')),
     };
 }
 

src/PVE/HA/Resources/PVEVM.pm
@@ -183,9 +183,10 @@ sub remove_locks {
 sub get_static_stats_from_config {
     my ($class, $conf) = @_;
 
+    # Ensure types are compatible with what Rust expects: f64 for maxcpu and usize for maxmem.
     return {
-        maxcpu => PVE::QemuConfig->get_derived_property($conf, 'max-cpu'),
-        maxmem => PVE::QemuConfig->get_derived_property($conf, 'max-memory'),
+        maxcpu => 0.0 + PVE::QemuConfig->get_derived_property($conf, 'max-cpu'),
+        maxmem => int(PVE::QemuConfig->get_derived_property($conf, 'max-memory')),
     };
 }
 